bdb884a0dc9ae06baf846372092b08796469cde3
raffiakhyari <muhammadraffi.akhyari@gmail.com>
2021-04-18 16:40:34 +0700

    backend foodmarket sudah selesai

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\Storage;
 
 class Food extends Model
@@ -13,30 +13,28 @@ class Food extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'name', 'description', 'ingredient',
-        'price', 'rate', 'types', 'picturePath'
+        'picturePath', 'name', 'description', 'ingredients', 'price', 'rate', 'types'
     ];
-
-    
-    public function getCreatedAtAttribute($value){
-        
-        return Carbon::parse($value)-> timestamp;
-    }
-
-    public function getUpdatedAtAttribute($value){
-        return Carbon::parse($value)-> timestamp;
-    }
 
     public function toArray()
     {
         $toArray = parent::toArray();
-        $toArray['picturePath'] = $this -> picturePath;
+        $toArray['picturePath'] = $this->picturePath;
         return $toArray;
     }
 
-    public function getPicturePathAttribute(){
-        return url(''). Storage::url($this->attributes['picturePath']);
+    public function getCreatedAtAttribute($created_at)
+    {
+        return Carbon::parse($created_at)
+            ->getPreciseTimestamp(3);
     }
-
+    public function getUpdatedAtAttribute($updated_at)
+    {
+        return Carbon::parse($updated_at)
+            ->getPreciseTimestamp(3);
+    }
+    public function getPicturePathAttribute()
+    {
+        return config('app.url') . Storage::url($this->attributes['picturePath']);
+    }
 }
-
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,44 +3,45 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'food_id', 'user_id', 'quantity',
-        'status', 'payment_url'
+        'food_id',
+        'user_id',
+        'quantity',
+        'total',
+        'status',
+        'payment_url'
     ];
 
-    public function food(){
-        return $this->hashOne(food::class, 'id', 'food_id'); //local key // foreign key
-    }
-    public function user(){
-        return $this->hashOne(user::class, 'id', 'user_id'); //local key // foreign key
-    }
-
-    public function getCreatedAtAttribute($value){
-        
-        return Carbon::parse($value)-> timestamp;
-    }
-
-    public function getUpdatedAtAttribute($value){
-        return Carbon::parse($value)-> timestamp;
-    }
-
-    public function toArray()
+    public function food()
     {
-        $toArray = parent::toArray();
-        $toArray['picturePath'] = $this -> picturePath;
-        return $toArray;
+        return $this->hasOne(Food::class,'id','food_id');
     }
 
-    // public function getPicturePathAttribute(){
-    //     return url(''). Storage::url($this->attributes['picturePath']);
-    // }
+    public function user()
+    {
+        return $this->hasOne(User::class,'id','user_id');
+    }
 
+    public function getCreatedAtAttribute($created_at)
+    {
+        return Carbon::parse($created_at)
+            ->getPreciseTimestamp(3);
+    }
+    public function getUpdatedAtAttribute($updated_at)
+    {
+        return Carbon::parse($updated_at)
+            ->getPreciseTimestamp(3);
+    }
 }
